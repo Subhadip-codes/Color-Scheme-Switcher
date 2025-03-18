03bf0c16cc6bad1b6d5ab03959b3f858c61c7533
Subhadip Layek <157273260+Subhadip-codes@users.noreply.github.com>
2025-03-19 01:07:45 +0530

    Add files via upload

---
+++ color_change.js
@@ -0,0 +1,31 @@
+const bottons=document.querySelectorAll('.button');
+const body=document.querySelector('body');
+
+bottons.forEach(function (button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==='grey')
+        {
+            body.style.background=e.target.id;/*#grey*/
+        } 
+        if(e.target.id==='white')
+        {
+            body.style.background=e.target.id;
+        }       
+        if(e.target.id==='blue')
+        {
+            body.style.background=e.target.id;
+        }
+        if(e.target.id==='yellow')
+        {
+            body.style.background=e.target.id;
+        }
+        if(e.target.id==='purple')
+        {
+            body.style.backgroundColor=e.target.id;
+        }
+    });
+    
+});
